793bacdffda3d40c8ad471589e781b84357c7aca
Mileta Dulović <dulovicmileta1@gmail.com>
2021-03-05 10:17:01 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,16 @@ import Head from 'next/head'
 import {useRouter} from "next/router"
 
 const Home = ()=> {
-	const router = useRouter()
+  const router = useRouter()
 
-	console.log(router)
+  console.log(router)
 
-	const setCookie = lang => {
-		document.cookie=`NEXT_LOCALE=${lang}`
-	}
+  const setCookie = lang => {
+    document.cookie=`NEXT_LOCALE=${lang}`
+  }
 
   return (
-		<button onClick={() => setCookie('en')}>Change cookie</button>
+    <button onClick={() => setCookie('en')}>Change cookie</button>
   )
 }
 
